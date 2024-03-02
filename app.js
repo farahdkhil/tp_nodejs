739d3cc0252b65289464c01651b4ff3d3ede9c1b
farahdkhil <farahdkhil000@gmail.com>
2024-03-02 08:47:34 +0100

    login post done

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const auth= require('./routes/auth')
 app.use(express.json());
 app.use('/post',post);
 app.use('/auth',auth);
-
+const dotenv = require('dotenv');
 const mongoose = require('mongoose')
-
+//bch yifhem ili huwe bch ya9ra ml fichier .env
+dotenv.config()
+const MONGODB_URI=process.env.MONGODB_URI
+// const PORT = process.env.PORT || 9000
+const PORT = process.env.PORT 
 //connection to the mongodb and start server
 mongoose.connect(MONGODB_URI).then(()=>{
     console.log('Connected to mongodb')
